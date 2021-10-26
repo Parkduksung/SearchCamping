fbc854bde1d3035e59adbd797a84fd6c4439d79f
Parkduksung <duksung1234@naver.com>
2021-10-26 23:02:48 +0900

    [feature] BookmarkFragment 에 기능 추가.

--- app/src/main/java/com/example/toycamping/ui/home/BookMarkFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.example.toycamping.ui.home
-
-import com.example.toycamping.R
-import com.example.toycamping.base.BaseFragment
-import com.example.toycamping.databinding.BookmarkFragmentBinding
-
-class BookMarkFragment : BaseFragment<BookmarkFragmentBinding>(R.layout.bookmark_fragment)
---
+++ app/src/main/java/com/example/toycamping/ui/home/BookmarkFragment.kt
@@ -0,0 +1,100 @@
+package com.example.toycamping.ui.home
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.toycamping.R
+import com.example.toycamping.base.BaseFragment
+import com.example.toycamping.base.ViewState
+import com.example.toycamping.databinding.BookmarkFragmentBinding
+import com.example.toycamping.room.entity.CampingEntity
+import com.example.toycamping.ui.adapter.BookmarkAdapter
+import com.example.toycamping.ui.adapter.viewholder.BookmarkListener
+import com.example.toycamping.viewmodel.BookmarkViewModel
+import com.example.toycamping.viewmodel.HomeViewModel
+
+class BookmarkFragment : BaseFragment<BookmarkFragmentBinding>(R.layout.bookmark_fragment),
+    BookmarkListener {
+
+    private val bookmarkAdapter by lazy { BookmarkAdapter() }
+
+    private val bookmarkViewModel by viewModels<BookmarkViewModel>()
+
+    private val homeViewModel by activityViewModels<HomeViewModel>()
+
+    override fun getItemClick(item: CampingEntity) {
+        homeViewModel.deleteBookmark(item)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        initUi()
+        initViewModel()
+
+    }
+
+    private fun initUi() {
+        startBookmarkAdapter()
+    }
+
+    private fun initViewModel() {
+        lifecycle.addObserver(bookmarkViewModel)
+
+        homeViewModel.viewStateLiveData.observe(viewLifecycleOwner) { viewState: ViewState? ->
+            (viewState as? HomeViewModel.HomeViewState)?.let {
+                onChangedHomeViewState(viewState)
+            }
+
+        }
+
+        bookmarkViewModel.viewStateLiveData.observe(viewLifecycleOwner) { viewState: ViewState? ->
+            (viewState as? BookmarkViewModel.BookmarkViewState)?.let {
+                onChangedBookmarkViewState(viewState)
+            }
+        }
+    }
+
+
+    private fun onChangedBookmarkViewState(viewState: BookmarkViewModel.BookmarkViewState) {
+        when (viewState) {
+            is BookmarkViewModel.BookmarkViewState.BookmarkList -> {
+                bookmarkAdapter.addAllBookmarkData(viewState.bookmarkList)
+            }
+            is BookmarkViewModel.BookmarkViewState.Error -> {
+                Toast.makeText(requireContext(), viewState.errorMessage, Toast.LENGTH_SHORT).show()
+            }
+
+            is BookmarkViewModel.BookmarkViewState.EmptyBookmarkList -> {
+
+            }
+        }
+    }
+
+
+    private fun onChangedHomeViewState(viewState: HomeViewModel.HomeViewState) {
+        when (viewState) {
+            is HomeViewModel.HomeViewState.AddBookmark -> {
+                bookmarkAdapter.addBookmark(viewState.item)
+            }
+            is HomeViewModel.HomeViewState.DeleteBookmark -> {
+                bookmarkAdapter.deleteBookmark(viewState.item)
+            }
+        }
+    }
+
+    private fun startBookmarkAdapter() {
+        binding.bookmarkRv.run {
+            adapter = bookmarkAdapter
+            bookmarkAdapter.clear()
+            layoutManager = LinearLayoutManager(requireContext())
+            bookmarkAdapter.setBookmarkItemClickListener(this@BookmarkFragment)
+        }
+        bookmarkViewModel.getAllBookmark()
+    }
+
+}
