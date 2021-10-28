775771afb5669164173255992dde671ac00028e2
Parkduksung <duksung1234@naver.com>
2021-10-28 17:20:05 +0900

    [feature] LoginFragment 추가.

---
+++ app/src/main/java/com/example/toycamping/ui/mypage/LoginFragment.kt
@@ -0,0 +1,52 @@
+package com.example.toycamping.ui.mypage
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.example.toycamping.R
+import com.example.toycamping.base.BaseFragment
+import com.example.toycamping.base.ViewState
+import com.example.toycamping.databinding.LoginFragmentBinding
+import com.example.toycamping.ext.showToast
+import com.example.toycamping.viewmodel.LoginViewModel
+
+class LoginFragment :
+    BaseFragment<LoginFragmentBinding>(R.layout.login_fragment) {
+
+    private val loginViewModel by viewModels<LoginViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = loginViewModel
+
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        loginViewModel.viewStateLiveData.observe(viewLifecycleOwner) { viewState: ViewState? ->
+            (viewState as? LoginViewModel.LoginViewState)?.let { onChangedLoginViewState(viewState) }
+        }
+    }
+
+    private fun onChangedLoginViewState(viewState: LoginViewModel.LoginViewState) {
+        when (viewState) {
+
+            is LoginViewModel.LoginViewState.EmptyUserId -> {
+                showToast("아이디를 입력해 주세요.")
+            }
+
+            is LoginViewModel.LoginViewState.EmptyUserPass -> {
+                showToast("패스워드를 입력해 주세요.")
+            }
+
+            is LoginViewModel.LoginViewState.LoginSuccess -> {
+                showToast("로그인을 성공하였습니다.")
+            }
+
+            is LoginViewModel.LoginViewState.LoginFailure -> {
+                showToast("로그인을 실패하였습니다.")
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/toycamping/viewmodel/LoginViewModel.kt
@@ -0,0 +1,60 @@
+package com.example.toycamping.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.MutableLiveData
+import com.example.toycamping.base.BaseViewModel
+import com.example.toycamping.base.ViewState
+import com.example.toycamping.data.repo.FirebaseRepository
+import com.example.toycamping.ext.ioScope
+import org.koin.java.KoinJavaComponent.inject
+
+class LoginViewModel(app: Application) : BaseViewModel(app) {
+
+    val userIdLiveData = MutableLiveData<String>()
+    val userPassLiveData = MutableLiveData<String>()
+
+    private val firebaseRepository by inject<FirebaseRepository>(FirebaseRepository::class.java)
+
+
+    fun login() {
+        checkLogin()?.let {
+            ioScope {
+                firebaseRepository.login(it.userId, it.userPass).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        viewStateChanged(LoginViewState.LoginSuccess)
+                    } else {
+                        viewStateChanged(LoginViewState.LoginFailure)
+                    }
+                }
+            }
+        }
+    }
+
+
+    private fun checkLogin(): User? {
+        return when {
+            userIdLiveData.value.isNullOrEmpty() -> {
+                viewStateChanged(LoginViewState.EmptyUserId)
+                null
+            }
+            userPassLiveData.value.isNullOrEmpty() -> {
+                viewStateChanged(LoginViewState.EmptyUserPass)
+                null
+            }
+            else -> {
+                User(userIdLiveData.value!!, userPassLiveData.value!!)
+            }
+        }
+    }
+
+    private data class User(val userId: String, val userPass: String)
+
+
+    sealed class LoginViewState : ViewState {
+        object EmptyUserId : LoginViewState()
+        object EmptyUserPass : LoginViewState()
+        object LoginSuccess : LoginViewState()
+        object LoginFailure : LoginViewState()
+    }
+
+}
