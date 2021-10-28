29912a37b711adc0a95f6d1d68241860cab45dbf
Parkduksung <duksung1234@naver.com>
2021-10-28 17:20:11 +0900

    [feature] MypageFragment 추가.

---
+++ app/src/main/java/com/example/toycamping/viewmodel/MyPageViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.toycamping.viewmodel
+
+import android.app.Application
+import com.example.toycamping.base.BaseViewModel
+
+class MyPageViewModel(app: Application) : BaseViewModel(app) {
+}
