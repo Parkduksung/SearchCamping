9754191d4b2aa3e8e07649024349963b5e600b7a
Parkduksung <duksung1234@naver.com>
2021-10-29 15:28:38 +0900

    [feature] dao 에서 불필요한 구문들 삭제.

--- app/src/main/java/com/example/toycamping/room/dao/CampingDao.kt
+++ app/src/main/java/com/example/toycamping/room/dao/CampingDao.kt
@@ -6,23 +6,6 @@ import com.example.toycamping.room.entity.CampingEntity
 @Dao
 interface CampingDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun registerCampingEntity(excelList: CampingEntity): Long
-
-    @Query("SELECT * FROM camping_table")
-    fun getAll(): List<CampingEntity>
-
-
-    @Query("UPDATE camping_table SET `like` = (:like) WHERE  name = (:name) AND address = (:address)")
-    fun updateBookmarkCampingData(
-        name: String,
-        address: String,
-        like: Boolean
-    ): Int
-
-    @Query("SELECT * FROM camping_table WHERE `like` = (:like)")
-    fun getBookmarkCampingEntity(like: Boolean = true): List<CampingEntity>
-
     @Query("SELECT * FROM camping_table WHERE `name` = (:name) ")
     fun getCampingEntity(name: String): CampingEntity
 
