34b0000939b1b4bc16cb88f0f28fa9ae630b2fdc
Parkduksung <duksung1234@naver.com>
2021-11-06 19:08:19 +0900

    [feature] BaseDialogFragment 추가.

---
+++ app/src/main/java/com/example/toycamping/base/BaseDialogFragment.kt
@@ -0,0 +1,28 @@
+package com.example.toycamping.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.DialogFragment
+
+abstract class BaseDialogFragment<B : ViewDataBinding>(@LayoutRes private val layoutId: Int) :
+    DialogFragment() {
+
+    protected lateinit var binding: B
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
+    }
+
+}
