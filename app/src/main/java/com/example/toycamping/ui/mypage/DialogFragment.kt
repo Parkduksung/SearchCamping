455d47c0cec6f99f3d19597230e0aca6939afd25
Parkduksung <duksung1234@naver.com>
2021-11-07 15:13:54 +0900

    [feature] DialogFragment 생성.

---
+++ app/src/main/java/com/example/toycamping/ui/mypage/DialogFragment.kt
@@ -0,0 +1,102 @@
+package com.example.toycamping.ui.mypage
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.viewModels
+import com.example.toycamping.R
+import com.example.toycamping.base.BaseDialogFragment
+import com.example.toycamping.base.ViewState
+import com.example.toycamping.databinding.DialogMypageBinding
+import com.example.toycamping.ext.showToast
+import com.example.toycamping.viewmodel.MyPageDialogViewModel
+
+class DialogFragment : BaseDialogFragment<DialogMypageBinding>(R.layout.dialog_mypage) {
+
+    private val myPageDialogViewModel by viewModels<MyPageDialogViewModel>()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        return dialog
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUi()
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+
+        binding.viewModel = myPageDialogViewModel
+
+        myPageDialogViewModel.viewStateLiveData.observe(viewLifecycleOwner) { viewState: ViewState? ->
+            (viewState as? MyPageDialogViewModel.MyPageDialogViewState)?.let {
+                onChangedMyPageDialogViewState(
+                    viewState
+                )
+            }
+        }
+    }
+
+    private fun onChangedMyPageDialogViewState(viewState: MyPageDialogViewModel.MyPageDialogViewState) {
+
+        when (viewState) {
+            is MyPageDialogViewModel.MyPageDialogViewState.ClickOK -> {
+                val getType = arguments?.getString(KEY_TYPE)
+                setFragmentResult(SUBMIT, bundleOf(TYPE to getType))
+                dismiss()
+            }
+
+            is MyPageDialogViewModel.MyPageDialogViewState.ClickNO -> {
+                dismiss()
+            }
+
+            is MyPageDialogViewModel.MyPageDialogViewState.Error -> {
+                showToast(message = viewState.message)
+            }
+        }
+    }
+
+
+    private fun initUi() {
+        with(binding) {
+            arguments?.run {
+                title = getString(KEY_TITLE, "")
+                detail = getString(KEY_DETAIL, "")
+                buttonTitle = getString(KEY_BUTTON_TITLE, "")
+            }
+        }
+    }
+
+    companion object {
+        const val SUBMIT = "submit"
+        const val TYPE = "type"
+
+        private const val KEY_TITLE = "TITLE"
+        private const val KEY_DETAIL = "DETAIL"
+        private const val KEY_BUTTON_TITLE = "BUTTON_TITLE"
+        private const val KEY_TYPE = "TYPE"
+
+        fun newInstance(
+            title: String,
+            detail: String,
+            buttonTitle: String,
+            type: String
+        ) =
+            DialogFragment().apply {
+                arguments = bundleOf(
+                    KEY_TITLE to title,
+                    KEY_DETAIL to detail,
+                    KEY_BUTTON_TITLE to buttonTitle,
+                    KEY_TYPE to type,
+                )
+            }
+    }
+}
