f0b631c36aca5eccff3f03f0360e6e322275042f
Parkduksung <duksung1234@naver.com>
2021-11-07 15:17:39 +0900

    [feature] Dialog 확장함수 생성.

--- app/src/main/java/com/example/toycamping/ext/ViewExt.kt
+++ app/src/main/java/com/example/toycamping/ext/ViewExt.kt
@@ -7,7 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentResultListener
 import com.example.toycamping.R
+import com.example.toycamping.ui.mypage.DialogFragment
 
 
 fun ConstraintLayout.showPOIInfoContainer(context: Context) {
@@ -36,6 +38,49 @@ fun ConstraintLayout.hidePOIInfoContainer(context: Context) {
 fun Fragment.showToast(context: Context = this.requireContext(), message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
+
+fun Fragment.showDialog(
+    title: String,
+    detail: String,
+    titleButton: String,
+    type: String,
+    listener: FragmentResultListener
+) {
+    val dialog = DialogFragment.newInstance(
+        title = title,
+        detail = detail,
+        buttonTitle = titleButton,
+        type = type
+    )
+    dialog.show(parentFragmentManager, dialog::class.simpleName)
+    parentFragmentManager.setFragmentResultListener(
+        DialogFragment.SUBMIT,
+        this,
+        listener
+    )
+}
+
+fun AppCompatActivity.showDialog(
+    title: String,
+    detail: String,
+    titleButton: String,
+    type: String,
+    listener: FragmentResultListener
+) {
+    val dialog = DialogFragment.newInstance(
+        title = title,
+        detail = detail,
+        buttonTitle = titleButton,
+        type = type
+    )
+    dialog.show(supportFragmentManager, dialog::class.simpleName)
+    supportFragmentManager.setFragmentResultListener(
+        DialogFragment.SUBMIT,
+        this,
+        listener
+    )
+}
+
 
 fun AppCompatActivity.showToast(context: Context = this, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
