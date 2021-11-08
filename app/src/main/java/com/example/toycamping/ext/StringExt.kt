d6a029e95a1e261a8aae6badcf3fe125f4ff392e
Parkduksung <duksung1234@naver.com>
2021-11-08 17:21:12 +0900

    [feature] String date 관련 ext 추가.

--- app/src/main/java/com/example/toycamping/ext/StringExt.kt
+++ app/src/main/java/com/example/toycamping/ext/StringExt.kt
@@ -7,3 +7,10 @@ fun String.getDate(): String {
     val day = date.substring(6, 8)
     return "$year.$month.$day"
 }
+
+fun String.convertDate() : String {
+    val year = this.substring(0, 4)
+    val month = this.substring(4, 6)
+    val day = this.substring(6, 8)
+    return "$year.$month.$day"
+}
